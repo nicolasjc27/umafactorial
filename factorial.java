be5cf7994b89fe57d5e42bc121d8213e69b8008d
nicolasjc27 <nicjilcan@uma.es>
2017-03-20 17:26:09 +0100

    refactorizacion de metodo de entrada

--- factorial.java
+++ factorial.java
@@ -1,23 +1,19 @@
-import java.util.Scanner;
-public class factorial {
-public static long fact(long num) {
-long fac = 1;
-for (long i=1; i<=num; ++i) {
-fac = fac * i;
-}
-return fac;
-}
-public static void main(String []args) {
+public static long getNumber() {
 Scanner scanInput = new Scanner(System.in);
 System.out.print("Introduzca un numero: ");
 long num = scanInput.nextLong();
+scanInput.close();
+return num;
+}
+public static void main(String []args) {
+long num = getNumber();
 if (num<0) {
-System.out.println("El factorial no esta definido"
-+ " para numeros negativos");
+System.out.println("El factorial no esta definido" +
+" para numeros negativos");
 } else {
 long fac = fact(num);
 System.out.println("El factorial de "+num+" es "+fac);
 }
-scanInput.close();
+}
 }
 }
